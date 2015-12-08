3b93640d67ac9681a6821faeb79b241f5affd9fb
Eric Langlois <eric@langlois.xyz>
2015-12-08 04:48:56 -0500

    Removed __ne__ from TypedEqualityMixin
    Python automatically defines != in terms of __eq__

--- comparison.py
+++ comparison.py
@@ -4,11 +4,7 @@
 class TypedEqualityMixin():
     """Equality based on type and members.
 
-    Defines == and != such that other is equal if type and __dict__ are the
-    same.
+    Defines == such that other is equal if type and __dict__ are the same.
     """
     def __eq__(self, other):
         return type(self) is type(other) and self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not self == other
